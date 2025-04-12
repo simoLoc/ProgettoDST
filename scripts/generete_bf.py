9931ad825f2b39bb67ac9b1788008f1f4df0305e
NapMarta <marta.napolillo@outlook.it>
2025-04-12 15:00:53 +0200

    Aggiunta liste per generazione del Belief State corrente

---
+++ scripts/generete_bf.py
@@ -0,0 +1,53 @@
+import  random
+
+# La struttura deve essere una lista per dare importanza all'ordine
+trigger = [['TC'], ['TC', 'TT'], ['TC', 'TT', 'TF'], ['TC', 'TT', 'TF', 'TFV']]
+action = [['AC'], ['AC', 'AT'], ['AC', 'AT', 'AF'], ['AC', 'AT', 'AF', 'AFV']]
+triggerAndAction = [['TC', 'AC'], ['TC', 'TT', 'AC'], ['TC', 'TT', 'AC', 'AT']]
+
+isAction = random.choice([0, 1])
+
+print("Genero il primo")
+if isAction:
+    fields = random.choice(triggerAndAction)
+    print(fields)
+else:
+    fields = random.choice(trigger)
+    print(fields)
+
+
+print("Genero le Trigger")
+
+# Generazione solo delle trigger (prima trigger e poi action)
+# devono essere 4 stringhe con 'T' -> cioè i 4 campi della trigger
+# while not (len(fields) >= 4 and sum(s.startswith('T') for s in fields) == 4):
+while not (sum(s.startswith('T') for s in fields) == 4):
+    new_fields = random.choice(trigger)    
+    print(f"inizio del ciclo {fields}")
+    print(f"Nuova generazione {new_fields}")
+    
+    if not set(new_fields).issubset(fields):
+        new_elements = set(new_fields) - set(fields)
+        new_elements = list(new_elements)
+        print(f'new_elements {new_elements}')
+        fields += new_elements
+        
+    print(f"NEW {fields}")
+
+    
+print("Genero le Action")
+while not (sum(s.startswith('A') for s in fields) == 4):
+    new_fields = random.choice(action)    
+    print(f"inizio del ciclo {fields}")
+    print(f"Nuova generazione {new_fields}")
+    
+    if not set(new_fields).issubset(fields):
+        new_elements = set(new_fields) - set(fields)
+        new_elements = list(new_elements)
+        print(f'new_elements {new_elements}')
+        fields += new_elements
+        
+    print(f"NEW {fields}")
+
+
+    
