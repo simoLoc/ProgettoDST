dba3e3aab2300cee8f0f83cd36aeca6a644cca0c
NapMarta <marta.napolillo@outlook.it>
2025-04-15 14:37:42 +0200

    update

--- scripts/generete_bf.py
+++ scripts/generete_bf.py
@@ -1,53 +1,59 @@
 import  random
+import json
+from tqdm import tqdm
 
 # La strutrigger_titleura deve essere una lista per dare importanza all'ordine
 trigger = [['trigger_channel'], ['trigger_channel', 'trigger_title'], ['trigger_channel', 'trigger_title', 'trigger_fields'], ['trigger_channel', 'trigger_title', 'trigger_fields', 'trigger_fields_values']]
 action = [['action_channel'], ['action_channel', 'action_title'], ['action_channel', 'action_title', 'action_fields'], ['action_channel', 'action_title', 'action_fields', 'action_fields_values']]
 triggerAndAction = [['trigger_channel', 'action_channel'], ['trigger_channel', 'trigger_title', 'action_channel'], ['trigger_channel', 'trigger_title', 'action_channel', 'action_title']]
 
-isAction = random.choice([0, 1])
 
-print("Genero il primo")
-if isAction:
-    fields = random.choice(triggerAndAction)
-    print(fields)
-else:
-    fields = random.choice(trigger)
-    print(fields)
+with open("dataset/values_prova_completo.json", "r", encoding="ISO-8859-1") as f:
+    dataset = json.load(f)
 
 
-print("Genero le Trigger")
+for i, entry in enumerate(tqdm(dataset, total=len(dataset))):
+    current_text = ""
+    isAction = random.choice([0, 1])
 
-# Generazione solo delle trigger (prima trigger e poi action)
-# devono essere 4 stringhe con 'T' -> cioè i 4 campi della trigger
-# while not (len(fields) >= 4 and sum(s.startswith('T') for s in fields) == 4):
-while not (sum(s.startswith('T') for s in fields) == 4):
-    new_fields = random.choice(trigger)    
-    print(f"inizio del ciclo {fields}")
-    print(f"Nuova generazione {new_fields}")
-    
-    if not set(new_fields).issubset(fields):
-        new_elements = set(new_fields) - set(fields)
-        new_elements = list(new_elements)
-        print(f'new_elements {new_elements}')
-        fields += new_elements
+    if isAction:
+        fields = random.choice(triggerAndAction)
+        print(fields)
+    else:
+        fields = random.choice(trigger)
+        print(fields)
+
+
+
+    # Generazione solo delle trigger (prima trigger e poi action)
+    # devono essere 4 stringhe con 'T' -> cioè i 4 campi della trigger
+    # while not (len(fields) >= 4 and sum(s.startswith('T') for s in fields) == 4):
+    while not (sum(s.startswith('T') for s in fields) == 4):
+        new_fields = random.choice(trigger)    
+        print(f"inizio del ciclo {fields}")
+        print(f"Nuova generazione {new_fields}")
         
-    print(f"NEW {fields}")
+        if not set(new_fields).issubset(fields):
+            new_elements = set(new_fields) - set(fields)
+            new_elements = list(new_elements)
+            print(f'new_elements {new_elements}')
+            fields += new_elements
+            
+        print(f"NEW {fields}")
 
-    
-print("Genero le Action")
-while not (sum(s.startswith('A') for s in fields) == 4):
-    new_fields = random.choice(action)    
-    print(f"inizio del ciclo {fields}")
-    print(f"Nuova generazione {new_fields}")
-    
-    if not set(new_fields).issubset(fields):
-        new_elements = set(new_fields) - set(fields)
-        new_elements = list(new_elements)
-        print(f'new_elements {new_elements}')
-        fields += new_elements
         
-    print(f"NEW {fields}")
+    while not (sum(s.startswith('A') for s in fields) == 4):
+        new_fields = random.choice(action)    
+        print(f"inizio del ciclo {fields}")
+        print(f"Nuova generazione {new_fields}")
+        
+        if not set(new_fields).issubset(fields):
+            new_elements = set(new_fields) - set(fields)
+            new_elements = list(new_elements)
+            print(f'new_elements {new_elements}')
+            fields += new_elements
+            
+        print(f"NEW {fields}")
 
 
     
