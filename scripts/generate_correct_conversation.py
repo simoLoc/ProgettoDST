4ca61d4b97a1849c2eb741ea391677d49a16d06f
NapMarta <marta.napolillo@outlook.it>
2025-04-30 10:13:00 +0200

    Aggiunta dei commenti

--- scripts/generate_correct_conversation.py
+++ scripts/generate_correct_conversation.py
@@ -8,6 +8,7 @@ import os
 name_model = "gemma-3-27b-it"
 model = lms.llm(name_model)
 
+# liste per la selezione dei campi da valutare
 trigger = [['trigger_channel'], ['trigger_channel', 'trigger_title'], ['trigger_channel', 'trigger_title', 'trigger_fields'], ['trigger_channel', 'trigger_title', 'trigger_fields', 'trigger_fields_values']]
 action = [['action_channel'], ['action_channel', 'action_title'], ['action_channel', 'action_title', 'action_fields'], ['action_channel', 'action_title', 'action_fields', 'action_fields_values']]
 triggerAndAction = [['trigger_channel', 'action_channel'], ['trigger_channel', 'trigger_title', 'action_channel'], ['trigger_channel', 'trigger_title', 'action_channel', 'action_title']]
@@ -18,9 +19,10 @@ with open("dataset/values_prova_completo.json", "r", encoding="utf-8") as f:
 
 
 for i, entry in enumerate(tqdm(dataset, total=len(dataset))):
-    current_text = ""
-    isAction = random.choice([0, 1])
+    current_text = ""   # stringa di output contenente tutta la conversazione
 
+    # scelta di iniziare o meno con i campi action
+    isAction = random.choice([0, 1])
     if isAction:
         fields = random.choice(triggerAndAction)
         print(fields)
@@ -28,6 +30,11 @@ for i, entry in enumerate(tqdm(dataset, total=len(dataset))):
         fields = random.choice(trigger)
         print(fields)
 
+    
+    # fields è una lista con i campi selezionati fino a quel momento 
+    # entry è la regola trigger-action presente nel dataset
+    # bf_current è un dizionario (chiave = nome del campo, valore = valore del campo)
+    # str_trigger_action_current è la formattazione in str dell'entry, solo per i campi contenuti in fields
     bf_current, str_trigger_action_current = get_prompt_input(fields, entry)
     
     prompt = get_prompt(isFirst = True, trigger_action_current = str_trigger_action_current, trigger_action_past = "")
@@ -38,7 +45,7 @@ for i, entry in enumerate(tqdm(dataset, total=len(dataset))):
     str_trigger_action_past = str_trigger_action_current
 
     # Generazione solo delle trigger (prima trigger e poi action)
-    # devono essere 4 stringhe con 'T' -> cioè i 4 campi della trigger
+    # devono essere 4 stringhe con 't' -> cioè i 4 campi della trigger
     # while not (len(fields) >= 4 and sum(s.startswith('T') for s in fields) == 4):
     while not (sum(s.startswith('t') for s in fields) == 4):
         new_fields = random.choice(trigger)    
@@ -46,7 +53,7 @@ for i, entry in enumerate(tqdm(dataset, total=len(dataset))):
         if not set(new_fields).issubset(fields):
             new_elements = set(new_fields) - set(fields)
             new_elements = list(new_elements)
-            fields += new_elements
+            fields += new_elements  # si tiene traccia dei campi correnti
 
             bf_new, str_trigger_action_current = get_prompt_input(new_elements, entry)
             
@@ -59,7 +66,7 @@ for i, entry in enumerate(tqdm(dataset, total=len(dataset))):
 
             str_trigger_action_past= str_trigger_action_current
 
-        
+    # devono essere 4 stringhe con 'a' -> cioè i 4 campi dell'action
     while not (sum(s.startswith('a') for s in fields) == 4):
         new_fields = random.choice(action)    
         
